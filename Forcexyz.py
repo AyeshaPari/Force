e4d4862feb93c68d85bc6cca1ba8446f2fa67276
SACRED GAMES 3.05 <106374961+YounisJohn@users.noreply.github.com>
2022-11-24 08:25:31 +0500

    Create Forcexyz.py

---
+++ Forcexyz.py
@@ -0,0 +1,49 @@
+import os
+
+ 
+
+ 
+
+ 
+
+if __name__ == "__main__":
+
+ 
+
+ 
+
+ 
+
+   try:
+
+ 
+
+ 
+
+ 
+
+       os.system("git pull")
+
+ 
+
+ 
+
+ 
+
+       __import__("Forcexyz").Younis_johnxyz()
+
+ 
+
+ 
+
+ 
+
+   except Exception as e:
+
+ 
+
+ 
+
+ 
+
+       exit(str(e))
